4b07c49f563c7c65cbe92f717413b1ca69dcc3e2
David Shen <46323153+davidyshen@users.noreply.github.com>
2022-01-21 09:46:50 -0500

    Commit on master

--- bot.py
+++ bot.py
@@ -1,6 +1,9 @@
 import slack
 import os
 import json
+import threading
+import glob
+import pandas as pd
 from datetime import datetime
 from pathlib import Path
 from dotenv import load_dotenv
@@ -16,40 +19,50 @@ slack_event_adapter = SlackEventAdapter(os.environ["SIGNING_SECRET"],"/slack/eve
 
 client = slack.WebClient(token=os.environ["SLACK_TOKEN"])
 
-client.chat_postMessage(channel="#coffee", text="☕ JuraBot 🤖 has reconnected...")
+client.chat_postMessage(channel="#jurabot-testing", text="☕ JuraBot 🤖 has reconnected...")
 BOT_ID = client.api_call("auth.test")["user_id"]
-
-# @slack_event_adapter.on("message")
-# def message(payload):
-#     event = payload.get("event", {})
-#     channel_id = event.get("channel")
-#     user_id = event.get("user")
-#     text = event.get("text")
-
-#     if BOT_ID != user_id:
-#         client.chat_postMessage(channel=channel_id, text=f"<@{user_id}>")
-
-    # print(event)
 
 # Report machine was cleaned with /cleaned
 @app.route("/cleaned", methods = ["POST"])
 def cleaned():
     data = request.form
-    print(data)
+    # print(data)
     team_id = data.get("team_id")
     user_id = data.get("user_id")
-    user_name = data.get("user_name")
     print("User_id = "+user_id)
+    user_name = client.users_profile_get(user=user_id).get("profile").get("real_name")
     channel_id = data.get("channel_id")
 
     # Check if record json exists for the team, if not create one
     if os.path.exists("./"+team_id+".json")==False:
-        open("./"+team_id+".json", "x")
+        jfile = open("./"+team_id+".json", "x")
+        record = {user_id:{"score" : 0, "user_name" : user_name}}
+    # Check if the record json is empty or not
+    elif os.stat("./"+team_id+".json").st_size == 0:
+        print("File empty, deleting and remaking...")
+        os.remove("./"+team_id+".json")
+        jfile = open("./"+team_id+".json", "x")
         record = {user_id:{"score" : 0, "user_name" : user_name}}
     else:
         # If json exists, load the json
-        jfile = open("./"+team_id+".json",)
+        print("Opening score json...")
+        jfile = open("./"+team_id+".json")
         record = json.load(jfile)
+    
+    # Check if the week record exsists, if not create one
+    if os.path.exists("./"+team_id+"_week.json")==False:
+        jfileWeek = open("./"+team_id+"_week.json", "x")
+        recordWeek = {user_id:{"score" : 0, "user_name" : user_name}}
+    elif os.stat("./"+team_id+"_week.json").st_size == 0:
+        print("File empty, deleting and remaking...")
+        os.remove("./"+team_id+"_week.json")
+        jfile = open("./"+team_id+"_week.json", "x")
+        record = {user_id:{"score" : 0, "user_name" : user_name}}
+    else:
+        # If json exists, load the json
+        print("Opening week score json...")
+        jfileWeek = open("./"+team_id+"_week.json")
+        recordWeek = json.load(jfileWeek)
 
     # If new user, add user id to the dictionary
     print(f"Is user in record: {user_id in record}")
@@ -57,14 +70,25 @@ def cleaned():
         print("New user, updating record...")
         tempDict = {user_id:{"score":0, "user_name" : user_name}}
         record.update(tempDict)
+    
+    # For week record
+    print(f"Is user in weekly record: {user_id in recordWeek}")
+    if (user_id in recordWeek) == False:
+        print("New user, updating weekly record...")
+        tempDict = {user_id:{"score":0, "user_name" : user_name}}
+        recordWeek.update(tempDict)
 
     print(f"Increasing {user_id}'s score by 1...")
     record[user_id]["score"] += 1
+    recordWeek[user_id]["score"] += 1
 
-    client.chat_postMessage(channel=channel_id, text=f'☕🤖⬅️🧽🚿 by <@{user_id}> at {datetime.now().strftime("%H:%M")}')
+    client.chat_postMessage(channel=channel_id, text=f'🧽➡️☕🤖 Jura Garbage Collection completed by {user_name} at {datetime.now().strftime("%H:%M")}')
+    client.chat_postMessage(channel=channel_id, text=f"{user_name}'s cleaning 💦🧼 score is now: {record[user_id]['score']} overall, {recordWeek[user_id]['score']} for the week 🎉")
 
-    print("Updating JSON file...")
-    json.dump(record, open("./"+team_id+".json", "w"))
+
+    print("Updating JSON files...")
+    json.dump(record, open("./"+team_id+".json","w"))
+    json.dump(recordWeek, open("./"+team_id+"_week.json","w"))
 
     return Response(), 200
 
@@ -73,19 +97,83 @@ def cleaned():
 @app.route("/leaderboard", methods = ["POST"])
 def leaderboard():
     data = request.form
-    print(data)
     team_id = data.get("team_id")
     user_id = data.get("user_id")
     print("User_id = "+user_id)
     channel_id = data.get("channel_id")
 
     jfile = open("./"+team_id+".json",)
+    jfileWeek = open("./"+team_id+"_week.json",)
     record = json.load(jfile)
+    recordWeek = json.load(jfileWeek)
+
+    sortRec = sorted(record, key=lambda x: (record[x]["score"]), reverse=True)
+    sortRecWeek = sorted(recordWeek, key=lambda x: (record[x]["score"]), reverse=True)
+
+    if (user_id in record):
+        client.chat_postMessage(channel=channel_id, text=f'You have {record[user_id]["score"]} points overall')
+    else:
+        client.chat_postMessage(channel=channel_id, text=f'You have no points. Maybe clean the coffee machine once?')
+    if (user_id in recordWeek):
+        client.chat_postMessage(channel=channel_id, text=f'You have {recordWeek[user_id]["score"]} points this week')
 
 
-    client.chat_postMessage(channel=channel_id, text=f'{record[user_id]["user_name"]}')
+    client.chat_postMessage(channel=channel_id, text=f'🏆 Weekly leaderboard 🏆')
+    for i in sortRecWeek:
+        client.chat_postMessage(channel=channel_id, text=f'{recordWeek[i]["user_name"]}: {recordWeek[i]["score"]} points')
+
+    client.chat_postMessage(channel=channel_id, text=f'🥉🥈🥇 Overall leaderboard (top 10 only) 🥇🥈🥉')
+    c = 0
+    for i in sortRec:
+        if c > 9: break
+        client.chat_postMessage(channel=channel_id, text=f'{record[i]["user_name"]}: {record[i]["score"]} points')
+        c += 1
+
     return Response(), 200
 
+# Alert people out of milk
+@app.route("/milk", methods = ["POST"])
+def milk():
+    data = request.form
+    print(data)
+    channel_id = data.get("channel_id")
+
+    client.chat_postMessage(channel=channel_id, text=f'<!channel> ☕🤖 JuraBot 🚫 OOM Error 🚨: Out Of Milk 🥛 🐮')
+    return Response(), 200
+
+
+def checkTime():
+    # Checks the day once a day
+    threading.Timer(86400, checkTime).start()
+
+    day = datetime.today().weekday()
+
+    teamInfo = client.team_info()
+    teamDetails = teamInfo.get("team")
+    team_id = teamDetails.get("id")
+
+    # For testing:
+    # day = 6
+
+    # Day = 6 = Sunday: Post and reset weekly leaderboard on sunday
+    if(day == 6):  # check if matches with the desired time
+        jfileWeek = open("./"+team_id+"_week.json",)
+        recordWeek = json.load(jfileWeek)
+        sortRecWeek = sorted(recordWeek, key=lambda x: (recordWeek[x]["score"]), reverse=True)
+        jfileWeek.close()
+        
+        client.chat_postMessage(channel="#jurabot-testing", text=f"🗓️ It's Sunday 😴: Weekly leaderboard placement 🥇🥈🥉 ")
+        for i in sortRecWeek:
+            client.chat_postMessage(channel="#jurabot-testing", text=f'{recordWeek[i]["user_name"]}: {recordWeek[i]["score"]} points')
+
+
+
+        weekJsons = glob.glob("*_week.json")
+        os.remove(weekJsons[0])
+        client.chat_postMessage(channel="#jurabot-testing", text=f"Leaderboard reset...💣💥")
+        
+
+checkTime()
 
 if __name__ == "__main__":
     app.run(debug=True)
